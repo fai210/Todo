cc401e01514750cc3dbdf882d5108acc3a6bb925
Fay Alshehri <fashehri@tetco.sa>
2024-10-23 10:44:38 +0300

    formeupdate

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,10 +1,11 @@
 export default function Footer() {
   return (
-    <div className="flex justify-between bg-white h-34 p-4">
-        <div className="font-bold text-xl ">Good Luke</div>
-        <img src="src/assets/to-do-list.png" className="w-11 h-13"></img>
-    </div>
-       
+    <div className=" bg-white h-34 p-4">
+        <div className="flex justify-between">
+            <div className="font-bold text-xl ">Good Luke</div>
+            <img src="src/assets/to-do-list.png" className="w-11 h-13"></img>
+        </div>
+    </div> 
     
   )
 }
--- src/component/SignIn.tsx
+++ src/component/SignIn.tsx
@@ -1,6 +1,37 @@
+import { useForm } from "react-hook-form"
+import { z } from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
+
+const loginvalidation=z.object({
+    firstName:z.string().min(7,"nnnnnnnn"), 
+    password:z.string().min(9,"Minimum length is 9"),
+}).refine((arg) =>arg.password != arg.password,{
+    message:"Invalid password",
+    path:["password"],
+})
+
+
+type loginvalidation =z.infer<typeof loginvalidation>
 
 export default function SignIn() {
+    const { register, handleSubmit ,formState: { errors }  } = useForm<loginvalidation>({resolver: zodResolver(loginvalidation)});
+    console.log(errors)
   return (
-    <div>SignIn</div>
+    <div className="space-y-5 pt-14  pb-4" >
+        <form className="flex flex-col  max-w-lg  mx-auto bg-slate-100 rounded-md p-5 space-y-6 " onSubmit={handleSubmit((data) => {
+                console.log(data);
+                
+            })}>
+            <label className="font-bold ">First Name:</label>
+            <input className="w-11/12 rounded-lg p-2" {...register("firstName")} type="text" placeholder="First Name" />
+            <p>{errors.firstName?.message}</p>
+               
+            <label className="font-bold ">Password:</label>
+            <input className="w-11/12 rounded-lg p-2" {...register("password" )} type="text" placeholder="Password" />
+            <p>{errors.password?.message}</p>
+
+            <input className="bg-black w-4/5 p-2 rounded-lg  text-white  hover:bg-slate-400" type="submit" ></input>
+        </form>
+    </div>
   )
 }
